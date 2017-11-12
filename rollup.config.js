14f53af541fdd930b22e526a250c66485953e2a5
Pedr Browne <pedr.browne@gmail.com>
2017-11-12 17:15:41 +0000

    fix: Missing exports for rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
 
 // Trasnpiled using Babel
 export default {
@@ -13,6 +14,16 @@ export default {
 
     babel({
       exclude: 'node_modules/**', // only transpile project source
+    }),
+
+    commonjs({
+      namedExports: {
+        // left-hand side can be an absolute path, a path
+        // relative to the current directory, or the name
+        // of a module in node_modules
+        'node_modules/is-plain-object/index.js': ['default'],
+        'node_modules/react/index.js': ['Component', 'createElement'],
+      },
     }),
   ],
 };
