3a36895b6fa6b792d6ded5f697606e8ad6e55329
Pedr Browne <pedr.browne@gmail.com>
2017-11-12 23:56:32 +0000

    docs: Remove uneeded comments form rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 
-// Trasnpiled using Babel
 export default {
   input: 'src/index.js',
   output: {
@@ -18,9 +17,6 @@ export default {
     }),
     commonjs({
       namedExports: {
-        // left-hand side can be an absolute path, a path
-        // relative to the current directory, or the name
-        // of a module in node_modules
         'node_modules/is-plain-object/index.js': ['default'],
         'node_modules/react/index.js': ['Component', 'createElement'],
       },
