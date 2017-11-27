b7ee134aeeb0ea01d39900a22600d4e37683ef40
Pedr Browne <pedr.browne@gmail.com>
2017-11-27 17:43:02 +0000

    test: Edit snapshots to expect media type for not

--- src/__tests__/query.js
+++ src/__tests__/query.js
@@ -176,7 +176,7 @@ describe('query', () => {
       ).toMatchSnapshot();
     });
 
-    it.only('allows mixed queries and not queries (both and and or)', () => {
+    it('allows mixed queries and not queries (both and and or)', () => {
       // @media not screen, not (color), not screen and (color) and (orientation: landscape) {
       const mq = mqWithValidBreakpointsForRange('width');
       const { grid, atWidth, query, not, colorGamut, orientation } = mq;
