c161f2647f0860388d43e61571cee4e325189b46
Pedr Browne <pedr.browne@gmail.com>
2017-11-30 19:21:34 +0000

    build: Move rollup-specific babel config into rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -9,18 +9,36 @@ export default {
     file: 'dist/styled-mq.js',
     format: 'umd',
   },
+  // Define modules that shoudln't be included in the build. It is assumed they
+  // will be available via globals at runtime.
   external: ['react', 'styled-components', 'ramda'],
   plugins: [
     nodeResolve(),
     babel({
+      babelrc: false,
       exclude: 'node_modules/**',
+      presets: [
+        [
+          'env',
+          {
+            modules: false,
+          },
+        ],
+      ],
+      plugins: [
+        'external-helpers',
+        'transform-es2015-destructuring',
+        'transform-object-rest-spread',
+      ],
     }),
     commonjs({
+      // Styled components uses the following and we need to ignore them here.
       namedExports: {
         'node_modules/is-plain-object/index.js': ['default'],
         'node_modules/react/index.js': ['Component', 'createElement'],
       },
     }),
   ],
+  // Define how external modules should be referenced in the UMD bundle
   globals: { react: 'React', 'styled-components': 'styled', ramda: 'R' },
 };
